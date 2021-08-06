ac4097bf3cab872bf00bdb598f90ec16929af0a8
Devansu <devansuyadav@gmail.com>
2021-08-06 22:12:23 +0530

    Update: Completed app logic + styled it with CSS

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,29 @@ const output = document.querySelector(".output");
 
 function checkForBirthdayAndLuckyNumber() {
     const dob = dateOfBirth.value;
+
     if(dob === "") {
         output.innerText = "You did not enter your Birthdate!!";
     } else {
-        checkBirthDatePalindrome(dob);
+        birthDateCombination(dob);
+    }
+}
+
+function birthDateCombination(dob) {
+    // split string
+    const splittedString = dob.split("-").reverse();
+
+    // mm-dd-yy
+    [splittedString[0], splittedString[1]] = [splittedString[1], splittedString[0]];
+    const mmddyy = splittedString[0] + splittedString[1] + splittedString[2].substring(2);
+
+    // m-dd-yyyy
+    const mddyyyy = splittedString[0].substring(1) + splittedString[1] + splittedString[2];
+
+    if(checkBirthDatePalindrome(dob) || checkBirthDatePalindrome(mmddyy) || checkBirthDatePalindrome(mddyyyy)) {
+        output.innerText = "Woww, Your Birthdate is a palindrome!🥳";
+    } else {
+        output.innerText = "Your Birthdate is not a palindrome!";
     }
 }
 
@@ -16,9 +35,9 @@ function checkBirthDatePalindrome(dob) {
     const reversedString = dob.split("").reverse().join("");
 
     if(reversedString === dob) {
-        output.innerText = "Your Birthday is a palindrome!";
+        return true;
     } else {
-        output.innerText = "Your Birthday is not a palindrome!";
+        return false;
     }
 }
 
